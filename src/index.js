0ef026d198adfb11443e5bbe9cd4a35bcace96f7
GmBeHappy <gmplk@hotmail.com>
2023-04-26 18:14:25 +0700

    add apm

--- src/index.js
+++ src/index.js
@@ -5,6 +5,23 @@ require('dotenv').config();
 
 const { Todo } = require('./models')
 
+// Add this to the VERY top of the first file loaded in your app
+var apm = require('elastic-apm-node').start({
+
+  // Override the service name from package.json
+  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+  serviceName: 'spa-server',
+
+  // Use if APM Server requires a secret token
+  secretToken: '3glfm7aYnC4YbuT8nQ',
+
+  // Set the custom APM Server URL (default: http://localhost:8200)
+  serverUrl: 'https://acdf5ddb4ad448bca620a5d6b914a674.apm.us-central1.gcp.cloud.es.io:443',
+
+  // Set the service environment
+  environment: 'my-environment'
+})
+
 const app = express();
 app.use(cors())
 
